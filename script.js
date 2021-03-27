51ddd7f4edbe9cf925262d7cd22dc7390e01e974
Sandeep GC <sandeep_gc@hotmail.com>
2021-03-28 10:25:45 +1100

    Questions arrays for the quiz

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+variable question1["1) Which type of JavaScript language is", "A. Object-Oriented", "B. Object-Based", "C. Assembly-language", "D. High-level", "Answer: B"];
+
+variable question2["2) Which one of the following also known as Conditional Expression:", "A. Alternative to if-else", "B. Switch statement", "C. If-then-else statement", "D. immediate if", "Answer: D"];
+
+variable question3["3)  In JavaScript, what is a block of statement?", "A. Conditional block", "B. block that combines a number of statements into a single compound statement", "C. both conditional block and a single statement", "D. block that contains a single statement", "Answer: B"];
+
+variable question4["4) Which of the following variables takes precedence over the others if the names are the same?", "A. Global variable", "B. The local element", "C. The two of the above", "D. None of the above", "Answer: B"];
+
+variable question5["5) Which one of the following is the correct way for calling the JavaScript code?", "A. Preprocessor", "B. Triggering Event", "C. RMI", "D. Function/Method", "Answer: D"];
+
+variable question6["6) Which of the following type of a variable is volatile?", "A. Mutable variable", "B. Dynamic variable", "C. Volatile variable", "D. Immutable variable", "Answer: A"];
 
 
 
