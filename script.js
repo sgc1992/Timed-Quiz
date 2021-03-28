ee03500c3c31bc2d8468c3e160c0dc1b4b4fdcdb
Sandeep GC <sandeep_gc@hotmail.com>
2021-03-28 13:26:20 +1100

    push check

--- script.js
+++ script.js
@@ -10,26 +10,26 @@ const questions =
 
 {
   question: "Which one of the following also known as Conditional Expression:",
-    choices : [A.Alternative to ifelse, B.Switch statement, C.If - then -else statement, D.immediate if],
+    choices : [A.Alternative to if else, B.Switch statement, C.If - then -else statement, D.immediate if],
   answer: D.immediate if
 }
 
 {
   question: "In JavaScript, what is a block of statement?",
-    choices : [A. Conditional block, B.block that combines a number of statements into a single compound statement, C.both conditional block and a single statement, D.block that contains a single statement],
-  answer: B.block that combines a number of statements into a single compound statement
+    choices : [A.Conditional block, B.block that combines a number of statements into a single compound statement, C.both conditional block and a single statement, D.block that contains a single statement],
+      answer: B.block that combines a number of statements into a single compound statement
 }
 
 {
   question: "Which of the following variables takes precedence over the others if the names are the same?",
-    choices : [A. Global variable, B.The local element, C.The two of the above, D.None of the above],
-  answer: B.block that combines a number of statements into a single compound statement
+    choices : [A.Global variable, B.The local element, C.The two of the above, D.None of the above],
+      answer: B.block that combines a number of statements into a single compound statement
 }
 
 {
   question: "Which one of the following is the correct way for calling the JavaScript code?",
-    choices : [A. Preprocessor, B.Triggering Event, C.RMI, D.Function/Method],
-  answer: D. Function/Method
+    choices : [A.Preprocessor, B.Triggering Event, C.RMI, D.Function / Method],
+      answer: D.Function / Method
 }
 
 
