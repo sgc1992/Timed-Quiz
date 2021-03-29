6744406f5b65fa7898eaffab0d379628e697d802
Sandeep GC <sandeep_gc@hotmail.com>
2021-03-29 11:19:11 +1100

    Added id for button and creted array for objects

--- script.js
+++ script.js
@@ -1,41 +1,45 @@
-document.querySelector('button').addEventListener('click',function presentQuestion(){
-//function defination
+document.querySelector('#start').addEventListener('click', function(){
+  timer();
+  presentQuestion();
+});
+
+function timer(){
+  
 }
 //call function
-presentQuestion();
-)
+function presentQuestion(){
+  alert("Ïnside present question function");
+}
+
 
 
 const questions =
-{
+[{
   question: "Which type of JavaScript language is",
   choices: "A.Object - Oriented, B.Object - Based, C.Assembly - language, D.High - level",
   answer: "B.Object - Based"
-}
+},
 
 {
   question: "Which one of the following also known as Conditional Expression:",
-    choices : "A.Alternative to if else, B.Switch statement, C.If - then -else statement, D.immediate if",
+  choices: "A.Alternative to if else, B.Switch statement, C.If - then -else statement, D.immediate if",
   answer: "D.immediate if"
-}
-
+},
 {
   question: "In JavaScript, what is a block of statement?",
     choices : "A.Conditional block, B.block that combines a number of statements into a single compound statement, C.both conditional block and a single statement, D.block that contains a single statement",
       answer: "B.block that combines a number of statements into a single compound statement"
-}
-
+},
 {
   question: "Which of the following variables takes precedence over the others if the names are the same?",
     choices : "A.Global variable, B.The local element, C.The two of the above, D.None of the above",
       answer: "B.block that combines a number of statements into a single compound statement"
-}
-
+},
 {
   question: "Which one of the following is the correct way for calling the JavaScript code?",
     choices : "A.Preprocessor, B.Triggering Event, C.RMI, D.Function / Method",
       answer: "D.Function / Method"
-}
+}]
 
 
 
