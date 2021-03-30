9784cca899070c071450e892ad2dae54b007ddb6
Sandeep GC <sandeep_gc@hotmail.com>
2021-03-31 06:34:20 +1100

    added changed the CSS

--- script.js
+++ script.js
@@ -53,7 +53,7 @@ function setTime() {
 
 // Function to send message
 function sendMessage() {
-  timeEl.textContent = "Time Finished";
+  timeEl.textContent = "Game is over";
 }
 
 
@@ -68,44 +68,9 @@ function showQuestions(){
     listItem.append(answerButton)
     answerOptions.append(listItem)
   }
-  // list answers
-  // allow users to choose an answer
+  
 }
 
-// function showQuestions(questions, quizContainer) {
-//   // we'll need a place to store the output and the answer choices
-//   var output = [];
-//   var answers;
-
-//   // for each question...
-//   for (var i = 0; i < questions.length; i++) {
-
-//     // first reset the list of answers
-//     answers = [];
-
-//     // for each available answer to this question...
-//     for (letter in questions[i].answers) {
-
-//       // ...add an html radio button
-//       answers.push(
-//         '<label>'
-//         + '<input type="radio" name="question' + i + '" value="' + letter + '">'
-//         + letter + ': '
-//         + questions[i].answers[letter]
-//         + '</label>'
-//       );
-//     }
-
-//     // add this question and its answers to the output
-//     output.push(
-//       '<div class="question">' + questions[i].question + '</div>'
-//       + '<div class="answers">' + answers.join('') + '</div>'
-//     );
-//   }
-
-//   // finally combine our output list into one string of html and put it on the page
-//   quizContainer.innerHTML = output.join('');
-// }
 
 
 
